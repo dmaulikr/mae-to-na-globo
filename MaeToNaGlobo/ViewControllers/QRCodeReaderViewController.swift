8513c3fdf984747232bc2041ff158e3b07e5f481
Daniel <danielcarlossmd@gmail.com>
2017-05-13 18:21:06 -0300

    Struct Qr code reader   - Add AVCodeReader   - Add QRCodeReaderViewModel   - Create UI for Found Program

--- MaeToNaGlobo/ViewControllers/QRCodeReaderViewController.swift
+++ MaeToNaGlobo/ViewControllers/QRCodeReaderViewController.swift
@@ -13,10 +13,49 @@ class QRCodeReaderViewController: UIViewController {
     @IBOutlet weak var qrCodeGuide: UIView!
     @IBOutlet weak var qrCodeFrame: UIView!
 
+    private var readingLayer: CALayer?
 
+    var qrCodeReader: CodeReader = AVCodeReader()
+    var viewModel = QRCodeReaderViewModel()
+
+    //MARK: ViewModel
+
+    private func bindInputToViewModel(_ code: String) {
+        viewModel.code = code
+    }
+
+    //MARK: life cycle
     override func viewDidLoad() {
         setupUI()
+        setupReadingLayer()
+        startReading()
     }
+
+    override func viewDidLayoutSubviews() {
+        readingLayer?.frame = qrCodeFrame.bounds
+    }
+
+    override func viewWillDisappear(_ animated: Bool) {
+        qrCodeReader.stopReading()
+    }
+
+
+    //MARK: --
+
+    private func startReading() {
+        qrCodeReader.startReading { [unowned self] (code) in
+            self.bindInputToViewModel(code)
+        }
+    }
+
+    private func setupReadingLayer() {
+        readingLayer = qrCodeReader.videoPreview
+
+        if let readingLayer = readingLayer {
+            qrCodeFrame.layer.addSublayer(readingLayer)
+        }
+    }
+
 
     private func setupUI() {
         makeQrCodeGuideBordersRounded()
---
+++ MaeToNaGlobo/ViewModels/QRCodeReaderViewModel.swift
@@ -0,0 +1,22 @@
+//
+//  QRCodeReaderViewModel.swift
+//  MaeToNaGlobo
+//
+//  Created by Daniel Carlos on 5/13/17.
+//  Copyright © 2017 Daniel Carlos. All rights reserved.
+//
+
+import Foundation
+
+
+class QRCodeReaderViewModel {
+
+    var code: String = "" {
+        didSet {
+            print("search on service")
+        }
+    }
+    
+
+
+}
