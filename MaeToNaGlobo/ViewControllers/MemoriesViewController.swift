4e6bfd8cfe64196713e3bf6e20c1bcdb387c436b
Daniel <danielcarlossmd@gmail.com>
2017-05-14 10:46:20 -0300

    Show and share video (url)   - use mocked video for now

--- MaeToNaGlobo/ViewControllers/MemoriesViewController.swift
+++ MaeToNaGlobo/ViewControllers/MemoriesViewController.swift
@@ -7,15 +7,52 @@
 //
 
 import UIKit
+import AVFoundation
+import AVKit
+
 
 class MemoriesViewController: UITableViewController {
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 10
+        return 1
     }
 
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        return tableView.dequeueReusableCell(withIdentifier: "MemoryCell")!
+        let cell = tableView.dequeueReusableCell(withIdentifier: "MemoryCell") as! MemoryCell
+
+        cell.share = { url in
+            let vc = UIActivityViewController(activityItems: [url], applicationActivities: [])
+            self.present(vc, animated: true, completion: nil)
+        }
+
+        return cell
     }
     
+}
+
+class MemoryCell: UITableViewCell {
+    @IBOutlet var videoFrame: UIView!
+
+    var playerController: AVPlayerViewController!
+    var share: ((String) ->Void)?
+
+    override func layoutSubviews() {
+        playerController.view.frame = videoFrame.bounds
+    }
+
+    override func awakeFromNib() {
+
+        playerController = AVPlayerViewController()
+        videoFrame.addSubview(playerController.view)
+
+        playerController.view.autoresizingMask = [.flexibleWidth, .flexibleHeight]
+
+        let url = URL(string: "http://jplayer.org/video/m4v/Big_Buck_Bunny_Trailer.m4v")!
+        playerController.player = AVPlayer(url: url)
+    }
+
+    @IBAction func shareIt() {
+        share?("http://jplayer.org/video/m4v/Big_Buck_Bunny_Trailer.m4v")
+    }
+
 }
